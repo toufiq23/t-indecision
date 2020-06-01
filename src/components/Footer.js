8a6f3ff6aa32d1d1f4cd2e763b49ff18da944b4c
toufiq23 <drrameraaron023@gmail.com>
2020-06-01 14:38:20 +0200

    ready for deployment

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = (props) => (
+  <div className="footer">
+		<div className="container">
+      <h2>Thanks for your visit.</h2>
+			<h3>Created by M Toufiq ELAHI</h3>
+    </div> 
+  </div>
+);
+
+export default Footer;
